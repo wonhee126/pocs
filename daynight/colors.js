1aea9d2e62a8488d356c5f0696b1e2d9584a10ec
ju wonhee <109426768+wonhee126@users.noreply.github.com>
2022-07-28 16:07:03 +0900

    Add files via upload
    화면 색상 전환

---
+++ daynight/colors.js
@@ -0,0 +1,30 @@
+var Links = {
+    setColor:function(color){ 
+  $('a').css('color',color);
+      
+    }
+  }
+  var Body = {
+    setColor:function(color){
+      document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor:function(color){
+      document.querySelector('body').style.backgroundColor = color;
+}
+  }
+  function nightDayHandler(self){
+var target = document.querySelector('body');
+if(self.value === 'night'){
+  Body.setBackgroundColor('black');
+  Body.setColor('white');
+  self.value = 'day';
+
+  Links.setColor('powderblue');
+} else{
+  Body.setBackgroundColor('white');
+  Body.setColor('black');
+  self.value = 'night';
+
+  Links.setColor('blue');
+}
+}
